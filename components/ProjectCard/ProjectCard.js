ba1c4a1b32f7e439e3da5424b510a016f981ea0b
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-07-22 00:34:37 +0200

    refact Latest Project

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -1,21 +1,14 @@
 import "./ProjectCard.css";
 export const ProjectCard = (project) => `
-<div class="project-card">
-<img src=${project.image} alt=${project.title}/>
-<div class="header">
-<h2>${project.title}</h2>
-<div>
-<a href=${project.github}>
-<img src="/icons/github.png" alt="GitHub Icon" />
-</a>
-<a href=${project.link}>
-<img src="/icons/link.png" alt="Link icon" />
-</a>
-</div>
-</div>
-<div class="detail">
-<p>${project.description}</p>
-<p class="tech">${project.tech.join(" - ")}</p>
-</div>
+<div class="portfolio-box">
+      <img src=${project.image} alt=${project.title}/>
+    <div class="portfolio-layer">
+      <h4>${project.title}</h4>
+      <p>${project.description}</p>
+      <a href=${project.link}>
+    </div>
+        <div class="detail">
+            <p class="tech">${project.tech.join(" - ")}</p>
+        </div>
 </div>
 `;
--- pages/Projects/Projects.js
+++ pages/Projects/Projects.js
@@ -8,9 +8,9 @@ cleanPage(main);
 main.innerHTML = `
 <section class="portfolio" id="portfolio">
 <h2 class="heading">Latest <span>Project</span></h2>
-<div class="projects-container"></div>
+<div class="portfolio-container"></div>
 </section>`;
-const container = document.querySelector(".projects-container");
+const container = document.querySelector(".portfolio-container");
 for (const project of projects) {
 const figure = document.createElement("figure");
 figure.innerHTML = ProjectCard(project);

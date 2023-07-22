4225be164ccf7e6ab1efc7697f57086dda3a5caf
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-07-22 03:55:35 +0200

    refact dark mode

--- main.js
+++ main.js
@@ -12,16 +12,24 @@ linkPage("#homelink", Home);
 linkPage("#aboutlink", About);
 linkPage("#projectslink", Projects);
 linkPage("#contactlink", Contact);
- 
+Home();
 
 /*========== sticky navbar ==========*/
 window.onscroll = () => {
-let header = document.querySelector('.header');
+    let header = document.querySelector('.header');
+    
+    header.classList.toggle('sticky', window.scrollY > 100);
+    };
 
-header.classList.toggle('sticky', window.scrollY > 100);
+/*========== dark light mode ==========*/
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
 };
 
-Home();
+
 
 
 

b71be06ba883c4fb50e337caa9f1d06c1c52a7c5
Admin <Manuel.jimena29@gmail.com>
2023-07-24 11:26:01 +0200

    refact animations

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,23 +1,23 @@
 import "./Header.css";
 
 export const Header = () => `
-<a href="#" class="logo">Portfolio</a>
-<nav class="navbar">    
- <ul>
-   <li>
-      <a href="#" class="active" id="homelink">Home</a>
-   </li>
-   <li>
-      <a href="#" id="aboutlink">About</a>
-   </li>
-   <li>
-      <a href="#" id="projectslink">Projects</a>
-   </li>
-   <li>
-      <a href="#" id="contactlink">Contact</a>
-   </li>
-   </ul>
-</nav>
-<div class="bx bx-moon" id="darkMode-icon"></div>
-<div class="bx bx-menu" id="menu-icon"></div>
+    <a href="#" class="logo">Portfolio</a>
+    <nav class="navbar">    
+      <ul>
+        <li>
+          <a href="#" class="active" id="homelink">Home</a>
+        </li>
+        <li>
+          <a href="#" id="aboutlink">About</a>
+        </li>
+        <li>
+          <a href="#" id="projectslink">Projects</a>
+        </li>
+        <li>
+          <a href="#" id="contactlink">Contact</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="bx bx-moon" id="darkMode-icon"></div>
+    <div class="bx bx-menu" id="menu-icon"></div>
 `;

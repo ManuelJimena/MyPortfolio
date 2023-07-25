d123907375ad7900328eefcebc5e025ef52f9f18
Admin <Manuel.jimena29@gmail.com>
2023-07-25 14:11:16 +0200

    refact animations

--- pages/NotFound/NotFound.js
+++ pages/NotFound/NotFound.js
@@ -3,11 +3,14 @@ import "./NotFound.css";
 const template = () => {
   return `
   <section id="not-found">
-    <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
+    <div id="title">404 Error Page Not Found</div>
     <div class="circles">
       <p>404<br>
        <small>PAGE NOT FOUND</small>
       </p>
+      <span class="circle big"></span>
+      <span class="circle med"></span>
+      <span class="circle small"></span>
     </div>
   </section>
 </div>
